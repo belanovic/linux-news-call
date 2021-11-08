0028b5d70a11cd02d6afe5d2cbd6d16537c123c7
Goran Belanovic <goranbelanovic@yahoo.com>
2021-11-08 03:11:20 +0100

    'msg'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const app = express();
 const http = require('http');
-///// const HOST_BACKEND = require('./hostBackend.js');
+////// const HOST_BACKEND = require('./hostBackend.js');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
@@ -19,10 +19,54 @@ const io = socketIO(server, {
 });
 
 io.on('connection', async (socket) => {
-    console.log('connected')
+
+    socket.on('create or join', (room) => {
+      const myRoom = io.sockets.adapter.rooms.get(room) || {size: 0};
+      
+    /*   if(io.sockets.adapter.rooms.get(room)) {
+        console.log(room)
+        socket.leave(room);
+      }  */
+
+      const numClientsRoom = myRoom.size;
+
+      if(numClientsRoom == 0) {
+        socket.join(room);
+        socket.emit('created', room);
+      } else if(numClientsRoom == 1) {
+        socket.join(room);
+        socket.emit('joined', room);
+      } else if (numClientsRoom == 2) {
+        socket.emit('full', room);
+      }
+    })
+
+    socket.on('leave', (room) => {
+      socket.leave(room);
+      console.log(room)
+    })
+
+
+    socket.on('ready', (room) => {
+      console.log('roosdfsdfsm');
+      socket.broadcast.to(room).emit('ready')
+    })
+    socket.on('candidate', (event) => {
+      socket.broadcast.to(event.room).emit('candidate', event)
+      console.log('evo ga kandidat')
+    })
+    socket.on('offer', (event) => {
+      console.log('evo me offer emit');
+      console.log(event.room);
+      socket.broadcast.to(event.room).emit('offer', event.sdp)
+    })
+    socket.on('answer', (event) => {
+      socket.broadcast.to(event.room).emit('answer', event.sdp)
+      console.log('evo ga answer')
+    })
 })
 
 const port = process.env.PORT || 4002;
-HOST_BACKEND = process.env.HOST_BACKEND || 'localhost';
+const HOST_BACKEND = process.env.HOST_BACKEND || 'localhost';
 server.listen(port, HOST_BACKEND, () => console.log(`Listening on port ${port}`));
 
